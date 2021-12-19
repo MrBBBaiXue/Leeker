26d4f06c4e1251db90509736c4725fc33f943ab1
MrBBBaiXue <wch272@outlook.com>
2021-12-19 20:54:58 +0800

    更新StockInfo

--- src/main/java/com/bakneko/App.java
+++ src/main/java/com/bakneko/App.java
@@ -6,5 +6,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        var stockYuTong = new StockInfo("宇通客车", "sh600066");
+        System.out.println(stockYuTong.GetCode() + " " + stockYuTong.GetName());
     }
 }
--- src/main/java/com/bakneko/StockInfo.java
+++ src/main/java/com/bakneko/StockInfo.java
@@ -1,5 +1,21 @@
 package com.bakneko;
 
+import java.util.Date;
+
 public class StockInfo {
-    
+    private String Name;
+    private String Code;
+    private StockData[] DayK;
+    public StockInfo(String name, String code)
+    {
+        Name = name;
+        Code = code;
+    }
+    public String GetName() { return Name; }
+    public String GetCode() { return Code; }
+    public void DownloadDayK(Date startDate)
+    {
+        // e.g. https://api.doctorxiong.club/v1/stock/kline/day?code=sh600066&startDate=2021-11-01
+
+    }
 }
